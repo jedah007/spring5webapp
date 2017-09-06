8fbeeaa3997a5c3a03c31348fe3b03a49ea74400
jens <j.dahlmanns@yahoo.de>
2017-09-06 22:02:56 +0200

    Adds repositories

---
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.spring5webapp.model.Book;
+
+/**
+ * @author Jens Dahlmanns
+ */
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
